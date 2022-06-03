424c6899b44b80e1c7f16f08b67008ff7fe00a02
Dang Nguyen Anh Khoa <kdang@axon.com>
2022-06-03 13:30:33 +0700

    use trading signal constants

--- mql5_python/abstract_strategy.py
+++ mql5_python/abstract_strategy.py
@@ -1,9 +1,13 @@
+from typing import Tuple
+
 import abc
 import pandas as pd
 
+from mql5_python.commons import TradingSignals
+
 
 class AbstractStrategy(abc.ABC):
-    def run(self):
+    def run(self) -> Tuple[TradingSignals, pd.DataFrame]:
         raise NotImplementedError()
 
     def init_df(self, df: pd.DataFrame):
